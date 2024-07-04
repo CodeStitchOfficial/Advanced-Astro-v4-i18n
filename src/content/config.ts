a2365c0ed6313491169f8de2d04b301eae41f010
Geoffrey le Meur <geoffrey.lemeur@gmail.com>
2024-07-04 19:19:57 +0200

    prettified

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,9 +1,7 @@
+// 1. Import utilities from `astro:content`
 import { z, defineCollection } from "astro:content";
 
-// Every collection must reflect Decap's config.yml collection schema
-// In order to be able to optimize images with Astro built-in compoments, like <Image />, we first must use this image helper 
-// Doc: https://docs.astro.build/en/guides/images/#images-in-content-collections
-
+// 2. Define a `type` and `schema` for each collection
 const blogsCollection = defineCollection({
 	type: "content",
 	schema: ({ image }) =>
@@ -12,12 +10,15 @@ const blogsCollection = defineCollection({
 			description: z.string(),
 			author: z.string(),
 			date: z.date(),
-            tags: z.array(z.string()),
+			tags: z.array(z.string()),
 			image: image(),
 			imageAlt: z.string(),
 		}),
 });
 
+// 3. Export a single `collections` object to register your collection(s)
+// Note: You can use defineCollection() as many times as you want to create multiple schemas. 
+// All collections must be exported from inside the single collections object.
 export const collections = {
 	blog: blogsCollection,
 };
--- src/data/navData.js
+++ src/data/navData.js
@@ -1,37 +1,37 @@
 const navData = [
-    {
-      key: "home",
-      url: "/",
-      children: []
-    },
-    {
-      key: "about",
-      url: "/about/",
-      children: []
-    },
-    {
-      key: "projects",
-      url: "/projects/",
-      children: [
-        {
-          key: "project-1",
-          url: "/projects/project-1/"
-        },
-        {
-          key: "project-2",
-          url: "/projects/project-2/"
-        }
-      ]
-    },
-    {
-      key: "blog",
-      url: "/blog/",
-      children: []
-    },
-    {
-      key: "contact",
-      url: "/contact/",
-      children: []
-    },
-  ]
+  {
+    key: "home",
+    url: "/",
+    children: []
+  },
+  {
+    key: "about",
+    url: "/about/",
+    children: []
+  },
+  {
+    key: "projects",
+    url: "/projects/",
+    children: [
+      {
+        key: "project-1",
+        url: "/projects/project-1/"
+      },
+      {
+        key: "project-2",
+        url: "/projects/project-2/"
+      }
+    ]
+  },
+  {
+    key: "blog",
+    url: "/blog/",
+    children: []
+  },
+  {
+    key: "contact",
+    url: "/contact/",
+    children: []
+  },
+]
 export default navData
