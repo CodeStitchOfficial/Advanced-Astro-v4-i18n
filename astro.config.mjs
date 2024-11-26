4b71ba9ed938267a4ae8096de97d67af86a771ef
Geoffrey Le Meur <geoffrey.lemeur@gmail.com>
2024-11-26 14:26:47 +0100

    feat: sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,28 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import icon from "astro-icon";
-
 import i18n from "@astrolicious/i18n";
+import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 export default defineConfig({
+  site: "https://www.yourwebsite.com", // update me!
   integrations: [
-    icon(), 
+    icon(),
     i18n({
       defaultLocale: "en",
       locales: ["fr", "en"],
       client: {
         data: true,
       },
-    })
-  ]
+    }),
+    sitemap({
+      i18n: {
+        defaultLocale: 'en', // All urls that don't contain `es` or `fr` after `"https://www.yourwebsite.com/"` will be treated as default locale, i.e. `en`
+        locales: {
+          // key/value pairs of all languages supported
+          en: 'en-US', // The `defaultLocale` value must be present in `locales` keys
+          fr: 'fr-FR',
+        },
+      },
+    }),
+  ],
 });
